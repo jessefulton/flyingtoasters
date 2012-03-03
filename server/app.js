289a01ccabc34f8f43c40fd1ffaf696a66dd1e10
jessefulton <none@none>
2012-03-03 13:43:51 -0800

    cleaning up routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,4 @@
-var http = require('http')
-	, sys = require('sys')
-	, url = require('url')
+var sio = require('socket.io')
 	, express = require('express');
 
 var app = express.createServer();
@@ -13,40 +11,8 @@ app.get('/', function(req,res) {
 	res.end("hello world");
 });
 
-app.get('/2', function(req,res) {
-	res.end("hello world2");
-});
 
-
-app.get('/proxy/:image_url(*)', function(req, res, next){
-//app.get('/proxied_image/:image_url', function(request_from_client, response_to_client){
-  sys.puts("Starting proxy");
-  
-  var image_url = req.params.image_url;
-
-  var image_host_name = url.parse(image_url).hostname
-  var filename = url.parse(image_url).pathname.split("/").pop()
-
-  var http_client = http.createClient(80, image_host_name);
-  var image_get_request = http_client.request('GET', image_url, {"host": image_host_name});
-  image_get_request.addListener('response', function(proxy_response){
-    var current_byte_index = 0;
-    var response_content_length = parseInt(proxy_response.header("Content-Length"));
-    var response_body = new Buffer(response_content_length);
-   
-    proxy_response.setEncoding('binary');
-    proxy_response.addListener('data', function(chunk){
-      response_body.write(chunk, current_byte_index, "binary");
-      current_byte_index += chunk.length;
-    });
-    proxy_response.addListener('end', function(){
-      res.contentType(filename);
-      res.send(response_body);
-    });
-  });
-  image_get_request.end();
-});
-
+require('./routes').init(app);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {

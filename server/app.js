056600e3a756a940affe49c818f1449252a73ca6
jessefulton <none@none>
2012-03-03 14:49:09 -0800

    adding package.json

--- server/app.js
+++ server/app.js
@@ -1,12 +1,55 @@
-var sio = require('socket.io')
-	, express = require('express');
+var express = require('express')
+	, stylus = require('stylus')
+	, nib = require('nib');
 
 var app = express.createServer();
 
 /**
+ * App configuration.
+ */
+app.configure(function () {
+	app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }))
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+	app.use(express.favicon());
+
+	// "app.router" positions our routes 
+	// above the middleware defined below,
+	// this means that Express will attempt
+	// to match & call routes _before_ continuing
+	// on, at which point we assume it's a 404 because
+	// no route has handled the request.
+	
+	app.use(app.router);
+
+	app.set('views', __dirname+"/views");
+
+	app.set('view engine', 'jade');
+
+	function compile (str, path) {
+		return stylus(str)
+			.set('filename', path)
+			.use(nib());
+	};
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+	app.use(express.logger('dev'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
+
+
+
+/**
  * GET serve when already rasterized.
  */
-
 app.get('/', function(req,res) {
 	res.end("hello world");
 });
--- server/routes/sockets.js
+++ server/routes/sockets.js
@@ -1,11 +1,35 @@
-//var ObjectId = require('mongoose').Types.ObjectId; 
-//var async = require('async');
-
+/**
+ * This handles the connection and dropping of websocket connections
+ */
 exports.init = function(app) {
+	var io = require('socket.io').listen(app)
+	io.set('log level', 1);
+	
+	
+	io.set('transports', [
+		'websocket'
+		, 'flashsocket'
+		, 'htmlfile'
+		, 'xhr-polling'
+		, 'jsonp-polling'
+	]);
+	
 
-	app.get('/socket', function(req,res) {
-		res.end("fubar");
+	io.sockets.on('connection', function (socket) {
+		socket.on('disconnect', function () {
+			console.log('disconnect');
+			//let's wait 2 seconds to see if player reconnects
+			setTimeout(function(){
+				console.log("Still disconnected? " + socket.connected + " " + socket.connecting);
+				if (!socket.connected && !socket.connecting) {
+					if (socket.partner) {
+						socket.partner.emit("game:end", PARTNER_DISCONNECT);
+					}
+					//disconnectPlayer(socket);
+				}
+			}, 2000);
+		});
+		
+
 	});
-
-	console.log("INSIDE SOCKETS");
 }; 

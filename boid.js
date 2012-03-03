b5e8cc31bf9ce3d11f3b51a51848e56b631a4b2a
willeastcott <will@playcanvas.com>
2012-03-03 13:45:25 -0800

    Added fog.

--- boid.js
+++ boid.js
@@ -1,11 +1,12 @@
 pc.script.create("boid", function (context) {
 
+    var extent = 10;
+
     var Boid = function (entity) {
         // Cache the entity that this script instance affects
         this.entity = entity;
         
         // Start at random location
-        var extent = 10;
         var ltm = this.entity.getLocalTransform();
         ltm[12] = Math.random() * 20 - 10;
         ltm[13] = Math.random() * 20 - 10;
@@ -16,9 +17,16 @@ pc.script.create("boid", function (context) {
         // Fly right to left
         var ltm = this.entity.getLocalTransform();
         ltm[12] += 0.1;
-        if (ltm[12] > 10) {
-            ltm[12] = -10;
+        if (ltm[12] > extent) {
+            ltm[12] = -extent;
         }
+        
+        var device = pc.gfx.Device.getCurrent();
+        device.updateGlobalState({
+            fog: true, 
+            fogDensity: 0.1,
+            fogColor: [0, 0, 0]
+        });
     };
 
     return Boid;

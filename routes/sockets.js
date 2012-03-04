3f6bc16b9c320346c4c085fd50a5430a338a157d
Jesse Fulton <jessefulton+github@gmail.com>
2012-03-03 22:34:42 -0800

    socket.io config for heroku

--- routes/sockets.js
+++ routes/sockets.js
@@ -6,7 +6,7 @@ exports.init = function(app) {
     var io = require('socket.io').listen(app);
     var User = require('../user.js');
 	io.set('log level', 1);
-	if (env.process == "production") {
+	if (process.env == "production") {
 		io.configure(function () { 
 		  io.set("transports", ["xhr-polling"]); 
 		  io.set("polling duration", 10); 

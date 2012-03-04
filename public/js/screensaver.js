720b91c1ceece089e3ecd0a22cd5644b544c9887
Jesse Fulton <jessefulton+github@gmail.com>
2012-03-04 11:48:22 -0800

    hooking in screensaver script

--- public/js/screensaver.js
+++ public/js/screensaver.js
@@ -1,75 +1,133 @@
+var _LOADED = false;
+window.playCanvasStatus = function(s) {
+	if (s >= 100) {
+		_LOADED = true;
+	}
+}
+
 $(function() {
-
-    //vars	
-	var state, counterID;	
-	var waitTime = 3000; // miliseconds
-
-	//constants
-	var states = {
-		'untouched': 1,
-		'active': 2,
-		'idle': 3
-	};
+	var nayGl = function() {
+		$(document.body).addClass("nogl");
+	}
 	
-	//TODO: we may want to limit the mousemove event (using setTimeout)
-	var interactionEvents = [
-		'mousemove',
-		'mousedown',
-		//'mouseup',
-		'keydown'
-	];
-
-
-
-	var init = function() {
-		state = states.untouched;
-
-		// register event listeners
-		interactionEvents.forEach( function (eventName) {
-			console.log(eventName);
-			$(document.body).on(eventName, interact); // ???
+	var initDesktop = function() {
+		$("#about-icon").on('click', function() {
+			$("#about").show();
 		});
-		startCounter();
+		$("#about .OK, #about .close").on('click', function() {
+			$("#about").hide();
+		});
+		$(document.body).addClass("loaded");
+		screenSave();
 	}
 
-	var interact = function() {
-		console.log(state);
-		if ( state == states.untouched ) {
+	var screenSave = function() {
+	
+		//vars	
+		var state, counterID;	
+		var waitTime = 3000; // miliseconds
+	
+		//constants
+		var states = {
+			'untouched': 1,
+			'active': 2,
+			'idle': 3
+		};
+		
+		//TODO: we may want to limit the mousemove event (using setTimeout)
+		var interactionEvents = [
+			'mousemove',
+			'mousedown',
+			//'mouseup',
+			'keydown'
+		];
+	
+	
+	
+		var init = function() {
+			state = states.untouched;
+	
+			// register event listeners
+			interactionEvents.forEach( function (eventName) {
+				console.log(eventName);
+				$(document.body).on(eventName, interact); // ???
+			});
 			startCounter();
-		} else if ( state == states.active ) {
-			resetCounter();
-		} else if ( state == states.idle ) {
-			endScreenSaver();
 		}
-		state = states.active;
-	};
-
-	var startCounter = function() {
-		counterID = setTimeout(startScreenSaver, waitTime);
+	
+		var interact = function() {
+			console.log(state);
+			if ( state == states.untouched ) {
+				startCounter();
+			} else if ( state == states.active ) {
+				resetCounter();
+			} else if ( state == states.idle ) {
+				endScreenSaver();
+			}
+			state = states.active;
+		};
+	
+		var startCounter = function() {
+			counterID = setTimeout(startScreenSaver, waitTime);
+		}
+	
+		var resetCounter = function() {
+			stopCounter();
+			startCounter();
+		}
+		
+		var stopCounter = function() {
+			clearTimeout(counterID);
+		}
+	
+		var startScreenSaver = function () {
+			console.log("STARTING SCREENSAVER");
+			state = states.idle;
+			stopCounter();
+			$("#application-container").show();
+			//load bootstrap.js
+		}
+	
+		var endScreenSaver = function () {
+			console.log("STOPPING SCREENSAVER");
+			startCounter();
+			$("#application-container").hide();
+		}
+	
+		init();
 	}
 
-	var resetCounter = function() {
-		stopCounter();
-		startCounter();
+	var loadScene = function(cb) {
+		jQuery.getScript("/bootstrap.js");
+		var boot = window.setInterval(function() {
+			if (_LOADED) {
+				window.clearInterval(boot);
+				cb();
+				console.log('\n');
+				console.log('**************************');
+				console.log('loaded');
+				console.log('**************************');
+				console.log('\n');
+			}
+		}, 250);
 	}
 	
-	var stopCounter = function() {
-		clearTimeout(counterID);
+	var yayGl = function() {
+		$(document.body).addClass("webgl");	
+		//loadScene(initDesktop);
+		window.setTimeout(function() {loadScene(initDesktop);}, 1000);
 	}
 
-	var startScreenSaver = function () {
-		console.log("STARTING SCREENSAVER");
-		state = states.idle;
-		stopCounter();
-		$("#application-container").show();
-		//load bootstrap.js
+
+	//Feature test WebGL
+	//via https://github.com/senchalabs/philogl/blob/master/src/webgl.js#L384-395			
+	try {
+		var canvas = document.createElement('canvas');
+		var hasGl = !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+		if (hasGl) { yayGl(); }
+		else { nayGl(); }
+	} catch(e) {
+		nayGl();
 	}
 
-	var endScreenSaver = function () {
-		console.log("STOPPING SCREENSAVER");
-		startCounter();
-		$("#application-container").hide();
-	}
-
-	init();
 });

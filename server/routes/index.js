a75048c6d1cadd041a2ef5e764803e07cefc0845
jessefulton <none@none>
2012-03-03 17:46:08 -0800

    removing jade and stylus

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,5 @@
 exports.init = function(app) {
 	require('./proxy').init(app);
 	require('./sockets').init(app);
-	
-	app.get('/', function(req, res){	  
-		res.render('index', { 
-			layout: false
-			, locals: { 
-				"bodyClasses": "index"
-				, "socketConnectUrl": app.set('siteUrl')
-			}
-		
-		});
-	});
+
 };
